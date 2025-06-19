857cd9ea91f38103e59d5eb118c8f0d14ff98514
Amirdhakadesan <sanjaykavish6@gmail.com>
2025-06-19 17:51:19 +0530

    Add files via upload

---
+++ Amirth.sql
@@ -0,0 +1,11 @@
+create database amirth;
+use amirth;
+
+
+
+Create table faculty(id INT auto_increment primary key, name varchar(100) Not Null, dept varchar(100) not null, title varchar(100) not null);
+
+
+select * from faculty;
+
+drop table admin;
